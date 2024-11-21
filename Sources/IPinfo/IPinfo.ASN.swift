2bd4a61170972eaf747e51343fed98cfcee55866
taylorswift <mdianna1989@gmail.com>
2024-11-21 17:59:16 -0600

    draft ASN APIs and ipinfo.io integration (#2)
    * draft ASN APIs and ipinfo.io integration
    * add a snippet demo-ing JSON decoding use case

---
+++ Sources/IPinfo/IPinfo.ASN.swift
@@ -0,0 +1,37 @@
+import Firewalls
+import IP
+import JSON
+
+extension IPinfo
+{
+    /// A helper type for parsing and formatting an ASN with the `AS` prefix.
+    struct ASN
+    {
+        let parsed:IP.ASN
+    }
+}
+extension IPinfo.ASN:CustomStringConvertible
+{
+    var description:String { "AS\(self.parsed)" }
+}
+extension IPinfo.ASN:LosslessStringConvertible
+{
+    init?(_ string:String)
+    {
+        guard
+        let i:String.Index = string.index(string.startIndex,
+            offsetBy: 2,
+            limitedBy: string.endIndex),
+        case "AS" = string[..<i],
+        let parsed:IP.ASN = .init(string[i...])
+        else
+        {
+            return nil
+        }
+
+        self.parsed = parsed
+    }
+}
+extension IPinfo.ASN:JSONStringDecodable, JSONStringEncodable
+{
+}
