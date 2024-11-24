4fb3b07d6a6f99c342adb6dd0beb90a07219e594
Diana <diana@rarestype.com>
2024-11-24 21:55:52 +0000

    more docs

--- Sources/Firewalls/IP.Claimant.swift
+++ Sources/Firewalls/IP.Claimant.swift
@@ -10,9 +10,13 @@ extension IP
         case github_webhook
         case github_other
 
+        /// Google’s “common” crawlers. This used to just be Googlebot, but has since been
+        /// co-opted for [Gemini AI](https://gemini.google.com/) training as well.
         case google_common
+        /// Google’s “special” crawlers, such as AdsBot.
         case google_special
 
+        /// Microsoft’s Bingbot. Microsoft claims that Bingbot is not used for AI training.
         case microsoft_bingbot
     }
 }
--- Sources/Firewalls/IP.Mapping.swift
+++ Sources/Firewalls/IP.Mapping.swift
@@ -3,6 +3,10 @@ import IP
 
 extension IP
 {
+    /// A data structure for efficient IP address lookups.
+    ///
+    /// IP mappings can be quite large, so you should always ensure the machine you are using
+    /// them on has enough memory to store them.
     @frozen public
     struct Mapping<Color>
     {
@@ -11,6 +15,13 @@ extension IP
         @usableFromInline
         var v6:Table<V6>
 
+        /// Constructs a mapping from lists of IP ranges and associated colors.
+        ///
+        /// The address ranges must be non-overlapping and sorted in ascending order. If not,
+        /// the initializer will throw an error.
+        ///
+        /// This does not perform any IPv6 to IPv4 normalization, which means IPv6-mapped IPv4
+        /// addresses in the `v6` list will never match any queries.
         public
         init(
             v4:[(ClosedRange<V4>, Color)] = [],
@@ -26,12 +37,23 @@ extension IP.Mapping:Sendable where Color:Sendable
 }
 extension IP.Mapping
 {
+    /// Performs a bisection search to look up the color associated with the given IP address,
+    /// using the IPv4 subtable only.
+    ///
+    /// >   Complexity:
+    ///     *O*(log *n*), where *n* is the number of ranges in the IPv4 subtable.
     @inlinable public
     subscript(v4 ip:IP.V4) -> Color?
     {
         self.v4.color(containing: ip)
     }
 
+    /// Performs a bisection search to look up the color associated with the given IP address.
+    /// If the IP address is an IPv6-mapped IPv4 address, the subscript will use the IPv4
+    /// subtable, otherwise it will use the IPv6 subtable.
+    ///
+    /// >   Complexity:
+    ///     *O*(log *n*), where *n* is the number of ranges in the corresponding subtable.
     @inlinable public
     subscript(v6 ip:IP.V6) -> Color?
     {
