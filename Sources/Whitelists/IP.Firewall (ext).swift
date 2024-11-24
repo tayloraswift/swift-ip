f684a85d56d77ebbc25d4499eda5c6459328f4ec
Dianna Ma <mdianna1989@gmail.com>
2024-11-24 23:04:09 +0000

    add example snippet, and disable conflicting GitHub whitelists

--- Sources/Whitelists/IP.Firewall (ext).swift
+++ Sources/Whitelists/IP.Firewall (ext).swift
@@ -12,26 +12,32 @@ extension IP.Firewall
     public mutating
     func attach(whitelist:GitHubWhitelist)
     {
-        var actions:IP.Claims<IP.Claimant> = .init(id: .github_actions)
+        // var actions:IP.Claims<IP.Claimant> = .init(id: .github_actions)
         var webhook:IP.Claims<IP.Claimant> = .init(id: .github_webhook)
-        var other:IP.Claims<IP.Claimant> = .init(id: .github_other)
+        // var other:IP.Claims<IP.Claimant> = .init(id: .github_other)
 
-        actions.extend(with: whitelist.actions)
-        actions.extend(with: whitelist.actionsMacOS)
+        // GitHub seems to use subsets of the GitHub Actions address space for its other
+        // services, which creates overlapping ranges. Some of the addresses are also claimed
+        // by Bingbot, I guess because Microsoft owns GitHub.
+
+        // actions.extend(with: whitelist.actions)
+        // actions.extend(with: whitelist.actionsMacOS)
 
         webhook.extend(with: whitelist.hooks)
 
-        other.extend(with: whitelist.web)
-        other.extend(with: whitelist.api)
-        other.extend(with: whitelist.git)
-        other.extend(with: whitelist.importer)
-        other.extend(with: whitelist.importerGitHubEnterprise)
-        other.extend(with: whitelist.packages)
-        other.extend(with: whitelist.pages)
-        other.extend(with: whitelist.dependabot)
+        // These also overlap with Bingbot...
 
-        self.claimants.append(actions)
+        // other.extend(with: whitelist.web)
+        // other.extend(with: whitelist.api)
+        // other.extend(with: whitelist.git)
+        // other.extend(with: whitelist.importer)
+        // other.extend(with: whitelist.importerGitHubEnterprise)
+        // other.extend(with: whitelist.packages)
+        // other.extend(with: whitelist.pages)
+        // other.extend(with: whitelist.dependabot)
+
+        // self.claimants.append(actions)
         self.claimants.append(webhook)
-        self.claimants.append(other)
+        // self.claimants.append(other)
     }
 }
